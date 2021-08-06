d1b555c7d87124040eb1f5bed360ba0a419241d1
Akshith <akshithnukala9@gmail.com>
2021-08-07 00:53:22 +0530

    mail authentication error dubuggeed

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Vlab]'
-    FLASKY_MAIL_SENDER = 'Rutherfordssstudio <rutherfordsstudio@gmail.com>'
+    FLASKY_MAIL_SENDER = 'Rutherfordssstudio'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
