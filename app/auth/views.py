d89af2296b7e77d05fced44ff635411fb0f22d39
Akshith <akshithnukala9@gmail.com>
2021-08-06 11:44:42 +0530

    semi developed version

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,16 +1,53 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from ..email import send_email
+from .forms import LoginForm, RegistrationForm
+
+def onLogin(loginFormObj,**kwarg):
+    # print(loginFormObj.data)
+    user = User.query.filter_by(email=loginFormObj.email.data.lower()).first()
+    if user is not None and user.verify_password(loginFormObj.password.data):
+        login_user(user, loginFormObj.remember_me.data)
+        next = request.args.get('next')
+        if next is None or not next.startswith('/'):
+            next = url_for('main.index')
+        return redirect(next)
+    flash('Invalid email or password.')
+    return render_template('login.html', loginFormObj=loginFormObj,**kwarg)
+
+def onReg(RegistrationFormObj,**kwarg):
+    user = User(email=RegistrationFormObj.email.data.lower(),
+            username=RegistrationFormObj.username.data,
+            password=RegistrationFormObj.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('You can now login.')
+    return redirect(url_for('/auth.index'))
+
+@auth.route('/', methods=['GET','POST'])
+def index(loginFormObj=None):
+    if(not loginFormObj):
+        loginFormObj = LoginForm()
+    RegistrationFormObj = RegistrationForm()
+    # print(dir(RegistrationFormObj.username))
+    # print(loginFormObj.validate_on_submit())
+    """ if loginFormObj.validate_on_submit():
+        return onLogin(loginFormObj,RegistrationFormObj=RegistrationFormObj)
+    if RegistrationFormObj.validate_on_submit():
+        return onReg(RegistrationFormObj,loginFormObj=loginFormObj) """
+    return render_template('login.html', loginFormObj=loginFormObj,
+        RegistrationFormObj=RegistrationFormObj)
+    # return render_template('auth/login.html', form=form)
 
 
-@auth.route('/', methods=['GET', 'POST'])
+@auth.route('/login', methods=['POST'])
 def login():
     loginFormObj = LoginForm()
-    print(loginFormObj.validate_on_submit())
+    RegistrationFormObj = RegistrationForm()
     if loginFormObj.validate_on_submit():
-        print(loginFormObj.data)
         user = User.query.filter_by(email=loginFormObj.email.data.lower()).first()
         if user is not None and user.verify_password(loginFormObj.password.data):
             login_user(user, loginFormObj.remember_me.data)
@@ -19,13 +56,21 @@ def login():
                 next = url_for('main.index')
             return redirect(next)
         flash('Invalid email or password.')
-    return render_template('login.html', loginFormObj=loginFormObj)
-    # return render_template('auth/login.html', form=form)
+    return render_template('login.html', loginFormObj=loginFormObj,RegistrationFormObj=RegistrationFormObj)
+    # return redirect(url_for('/auth.index',loginFormObj=loginFormObj))
 
+
+@auth.route('/register', methods=['POST'])
+def register():
+    loginFormObj = LoginForm()
+    RegistrationFormObj = RegistrationForm()
+    if RegistrationFormObj.validate_on_submit():
+        return onReg(RegistrationFormObj,loginFormObj=loginFormObj) 
+    return redirect(url_for('/auth.index'))
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
-    return redirect(url_for('main.index'))
+    return redirect(url_for('/auth.index'))
--- readme.md
+++ readme.md
@@ -3,6 +3,9 @@
 - [General info](#general-info)
 - [Technologies](#technologies)
 - [Setup](#setup)
+- [in Windows:](#in-windows)
+- [Html file replacemetns:](#html-file-replacemetns)
+- [Optional Changes:](#optional-changes)
 
 ## General info
 Intial implementation just rendering html pages.
@@ -23,18 +26,19 @@ Heroku Setup process
 ```
 
 
-in Windows:
+## in Windows:
     time.clock=>time.perf_counter
 
 
-Html file replacemetns:
+## Html file replacemetns:
     use cdn based bootstrap imports only....
     1) "assets/     =>        "static/
     2) \?h=[^> ]*>         =>    ">
     3) \?h=.*&quot    =>   &quot
     4) &quot;assets/   =>   &quot/static/
 
-
+## Optional Changes:
+    1) form valiidation tooltip https://getbootstrap.com/docs/4.3/components/forms/#tooltips
 
 Doubted changes 
     1)(not done https://www.geeksforgeeks.org/svg-viewbox-attribute/#:~:text=The%20viewBox%20is%20an%20attribute,well%20as%20width%20and%20height. ) index.html       line 178    viewbox =>  viewBox
