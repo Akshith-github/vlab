ef9f7cd62ecd4d6917f0de9f76fc7acea0f9394a
Akshith <akshithnukala9@gmail.com>
2021-08-07 00:42:26 +0530

    Account Confirmation

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,19 +8,28 @@ from .forms import LoginForm, RegistrationForm
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated:
-        if  request.path in ['/auth/login','/auth/','/auth/register']:
+    if current_user.is_authenticated and current_user.confirmed:
+        if  request.path in ['/auth/login','/auth/','/auth/register','/auth/unconfirmed']:
             flash("User already logged in")
             flash("Log out to visit the authentication page!!")
             return redirect(url_for('main.index'))
-            # return render_template('index.html',pageDashboard="active" )
-            # current_user.ping()
-            # if not current_user.confirmed \
-            #         and request.endpoint \
-            #         and request.blueprint != 'auth' \
-            #         and request.endpoint != 'static':
-            #     return redirect(url_for('auth.unconfirmed'))
-            
+    if current_user.is_authenticated \
+        and not current_user.confirmed \
+        and request.endpoint \
+        and request.blueprint != 'auth' \
+        and request.endpoint != 'static'\
+        and request.path not in ['/auth/unconfirmed','/auth/logout','/auth/confirm']\
+        and '/auth/confirm/' not in request.path:
+        print(request.path,"\n"*4)
+        return redirect(url_for('/auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('unconfirmed.html')
+
 
 
 @auth.route('/', methods=['GET','POST'])
@@ -69,7 +78,11 @@ def register():
                 password=RegistrationFormObj.password.data)
             db.session.add(user)
             db.session.commit()
-            flash('You can now login.')
+            flash('You can now login to Confirm Your Account')
+            token = user.generate_confirmation_token()
+            send_email(user.email, 'Confirm Your Account',
+                    'mail/confirm', user=user, token=token)
+            flash('A confirmation email has been sent to you by email.')
             return redirect(url_for('/auth.index'))
         flash("registration failed")
     return render_template('login.html', loginFormObj=loginFormObj,RegistrationFormObj=RegistrationFormObj,
@@ -82,6 +95,30 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('/auth.index'))
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+                'mail/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    # input()
+    print('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
 
 """ def onLogin(loginFormObj,**kwarg):
     # print(loginFormObj.data)
