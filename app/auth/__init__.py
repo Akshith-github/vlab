37dacfd675c0533347423b68388a3243d1d67bff
Akshith <akshithnukala9@gmail.com>
2021-08-07 23:43:13 +0530

    '/auth' to 'auth' auth blueprint name correction

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -3,7 +3,7 @@ import os
 
 
 # static_folder=
-auth = Blueprint('/auth', __name__,
+auth = Blueprint('auth', __name__,
     static_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)),"static"))
 
 # print(auth.static_folder)
