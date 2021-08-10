04cd1383f23849792628300eadb0b11a59a71dec
Akshith <akshithnukala9@gmail.com>
2021-08-11 01:31:37 +0530

    baby stage changes createcourse routes and lining and side and topnavbar

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,9 +1,32 @@
+inAuthPage=function(){
+  if(window.location.pathname.startsWith("/auth/") && !window.location.pathname.startsWith("/auth/reset") ){
+    console.log("in auth page");
+    return true
+  }
+  else{
+    console.log("Not in auth page");
+    return false
+  }
+}
 $(".goToFor").on("click", function(){
-  $('#carousel-1').carousel(0)});
+  if(inAuthPage()){
+  $('#carousel-1').carousel(0)}
+  else{
+    window.document.location="/auth"
+  }
+});
 $(".goToLogin").on("click", function(){
-    $('#carousel-1').carousel(1)});
+  if(inAuthPage()){
+    $('#carousel-1').carousel(1)}
+    else{
+      window.document.location="/auth/login"
+    }});
 $(".goToReg").on("click", function(){
-    $('#carousel-1').carousel(2)});
+  if(inAuthPage()){
+    $('#carousel-1').carousel(2)}
+    else{
+      window.document.location="/auth/register"
+    }});
 // $(document).ready(function(){
 //     $('#carousel-1').carousel(1)});
 
