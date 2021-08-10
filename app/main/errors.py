38932804e38b5636b5762b8ef2a8b4155ad80dde
Akshith <akshithnukala9@gmail.com>
2021-08-10 18:47:53 +0530

    errorPages

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from . import main
 
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template('customError.html',errorCode="403",errorName="Not authorized!! Forbidden!",errorQuote="🤔 Looks like you have landed into wrong place!!⛔"), 403
 
 @main.app_errorhandler(404)
 def page_not_found(e):
@@ -9,4 +12,4 @@ def page_not_found(e):
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('customError.html',errorCode="500",errorName="Internal Server Error",errorQuote="Lookslike We have issue on our side 😥"), 500
