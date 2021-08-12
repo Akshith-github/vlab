14eaf91c03198174a800578cc26e4d4d261b35ee
Akshith <akshithnukala9@gmail.com>
2021-08-12 12:44:33 +0530

    insert Roles

--- app/models.py
+++ app/models.py
@@ -9,12 +9,32 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'Student': [],
+            'Teacher': [],
+            'CoAdmin':[],
+            'Admin': [],
+        }
+        default_role = 'Student'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            # role.reset_permissions()
+            # for perm in roles[r]:
+            #     role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
 
     def __repr__(self):
         return '<Role %r>' % self.name
-
-
+    
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -23,6 +43,17 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(name='Admin').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+        # if self.email is not None and self.avatar_hash is None:
+        #     self.avatar_hash = self.gravatar_hash()
+        # self.follow(self)
 
     @property
     def password(self):
