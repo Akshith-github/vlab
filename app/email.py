cfa57331b00aee10b979a9bf38efee7547dde253
Akshith <akshithnukala9@gmail.com>
2021-08-12 15:31:54 +0530

    email changes finalized  to auto confirm user onthe set of CONFIRMUSER_ON_MAILFAIL to  on / 1 / true

--- app/email.py
+++ app/email.py
@@ -6,69 +6,76 @@ from .models import User
 import os
 import smtplib
 from email.message import EmailMessage
+def sendMail1(app, msg,to,subject):
+    with app.app_context():
+        print('MAIL_USERNAME',app.config.get('MAIL_USERNAME'))
+        print(app.config.get('MAIL_PASSWORD'))
+        print(os.environ.get('MAIL_USERNAME'))
+        mail.send(msg)
+        print("sent mail to ",to,"for",subject)
+    return True
 
+def sendMail2(EMAIL_ADDRESS,EMAIL_PASSWORD,mesg):
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(mesg)
+    return True
+    
+def sendMail3(app,msg,to,subject,EMAIL_ADDRESS,EMAIL_PASSWORD):
+    server = smtplib.SMTP('smtp.gmail.com', 25)
+    server.connect("smtp.gmail.com",465)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    text = msg.as_string()
+    server.sendmail(EMAIL_ADDRESS, to, msg.body)
+    server.quit()
+    print("unable to send mails")
+    return True
 
 def send_async_email(app, msg,to,subject):
+    flag=False
     try:
-        with app.app_context():
-            print('MAIL_USERNAME',app.config.get('MAIL_USERNAME'))
-            print(app.config.get('MAIL_PASSWORD'))
-            print(os.environ.get('MAIL_USERNAME'))
-            mail.send(msg)
-            print("sent mail to ",to,"for",subject)
+        flag=sendMail1(app, msg,to,subject)
     except Exception as e:
+        print("Failed to send message",str(e))
+    if(not flag):
         try:
             EMAIL_ADDRESS = os.environ.get('MAIL_USERNAME')
             EMAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
             # contacts = ['YourAddress@gmail.com', 'test@example.com']
-
             mesg = EmailMessage()
             mesg['Subject'] = subject
             mesg['From'] = EMAIL_ADDRESS
             mesg['To'] = to
-
             mesg.set_content(msg.body)
-
             mesg.add_alternative(msg.html, subtype='html')
-
-
-            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-                smtp.send_message(mesg)
-
-        except Exception as e1:
-            print(str(e1))
-            try:
-                server = smtplib.SMTP('smtp.gmail.com', 25)
-                server.connect("smtp.gmail.com",465)
-                server.ehlo()
-                server.starttls()
-                server.ehlo()
-                server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-                text = msg.as_string()
-                server.sendmail(EMAIL_ADDRESS, to, msg.body)
-                server.quit()
-                print("unable to send mails")
-            except Exception as e2:
-                print(str(e2))
-                with open("errors.txt","w") as f:
-                    f.write("\n\n\nFailed to send mails")
-                    try:
-                        f.write(str(e))
-                        f.write(str(e1))
-                    except Exception as e3:
-                        print("unable to write error mails",str(e3))
-                print("unable to send mails")
-                cnfrm_on_failure=os.environ.get('CONFIRMUSER_ON_MAILFAIL').lower() in ['true', 'on', '1']
+            flag=sendMail2(EMAIL_ADDRESS,EMAIL_ADDRESS,mesg)
+        except Exception as e:
+            print("Failed to send message",str(e))
+            if(not flag):
+                try:
+                    flag=sendMail3(app,msg,to,subject,EMAIL_ADDRESS,EMAIL_PASSWORD)
+                except Exception as e1:
+                    print("Failed to send message",str(e1))
+        if(not flag):
+            """ with open("errors.txt","w") as f:
+                f.write("\n\n\nFailed to send mails")
+                try:
+                    f.write(str(e))
+                    f.write(str(e1)) """
+            print("unable to send mails")
+            with app.app_context():
+                cnfrm_on_failure=os.environ.get('CONFIRMUSER_ON_MAILFAIL',"false").lower() in ['true', 'on', '1']
                 if(not User.query.filter_by(email=to).first().confirmed and cnfrm_on_failure):
                     User.query.filter_by(email=to).first().confirmed=True
-                    db.session.add(User.query.filter_by(email=to))
+                    db.session.add(User.query.filter_by(email=to).first())
                     db.session.commit()
-                    print("Unable to send mail to",User.query.filter_by(email=to).first().name)
+                    print("Unable to send mail to",User.query.filter_by(email=to).first().username)
                     print("Confirmed User")
 
-        
+
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
